9418c8b8cde9c95d954889b7c185e9a016af7e4c
stevema <chunlinpage@qq.com>
2012-11-02 14:49:53 +0800

    add Photo model

--- iWeibo/Photo.h
+++ iWeibo/Photo.h
@@ -11,5 +11,41 @@
 @interface Photo : BaseModel
 
 @property(nonatomic,strong)NSString *url;
+@property (nonatomic, strong)NSString *path;
+@property (nonatomic, strong)UIImage *image;
+
+/*!
+ *  @abstract initialize with data
+ *
+ *  @discussion
+ *	This method is used to initialize photo with provided data
+ *
+ *  @param data
+ *      dict data of photo
+ */
+-(id)initWithData:(NSDictionary *)data;
+
+/*!
+ *  @abstract initialize with data
+ *
+ *  @discussion
+ *	This method is used to initialize photo with provided data
+ *
+ *  @param data
+ *      dict data of photo
+ */
+-(id)initWithData:(NSDictionary *)data
+     autoDownload:(BOOL) flag;
+
+/*!
+ *  @abstract download photo
+ *
+ *  @discussion
+ *	This method is used to download the photo
+ *
+ */
+-(void)download:(RequestCompleteBlock) completionHandler
+onDownloadProgressChanged:(MKNKProgressBlock) downloadProgressHandler
+        onError:(RequestErrorBlock) errorHandler;
 
 @end
--- iWeibo/Photo.m
+++ iWeibo/Photo.m
@@ -10,4 +10,63 @@
 
 @implementation Photo
 
+@synthesize url = _url;
+@synthesize path = _path;
+@synthesize image = _image;
+
+-(id)initWithData:(NSDictionary *)data;
+{
+    self = [super init];
+    
+    if (self)
+    {
+        _url = [data valueForKey:@"url"];
+    }
+    
+    return self;
+}
+
+-(id)initWithData:(NSDictionary *)data
+     autoDownload:(BOOL) flag;
+{
+    self = [self initWithData:data];
+    if (self && flag == YES)
+    {
+        [self download:^(NSDictionary *data)
+         {
+             
+         }
+onDownloadProgressChanged:^(double progress)
+         {
+             
+         }
+               onError:^(NSString *msg)
+         {
+         }];
+    }
+    
+    return self;
+}
+
+-(void)download:(RequestCompleteBlock) completionHandler
+onDownloadProgressChanged:(MKNKProgressBlock) downloadProgressHandler
+        onError:(RequestErrorBlock) errorHandler;
+{
+    MKNetworkOperation *op = [self.api imageAtURL:[[NSURL alloc] initWithString:_url]
+                                     onCompletion:^(UIImage *fetchedImage, NSURL *url, BOOL isInCache)
+                              {
+                                  if([_url isEqualToString:[url absoluteString]]) {
+                                      _image = fetchedImage;
+                                      completionHandler(self.api.successObject);
+                                  }
+                              }
+                                          onError:^(NSError *error){
+                                              errorHandler(@"Failed to download image.");
+                                          }];
+    
+    [op onDownloadProgressChanged:^(double progress)
+     {
+         downloadProgressHandler(progress);
+     }];
+}
 @end
